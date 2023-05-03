b0628dac21179bc2038c3bd2d27b14b8d191e5ae
JlsBssmnn <julius.bossmann@mailbox.tu-dresden.de>
2023-05-03 09:43:00 +0200

    create config v3.0.6
    changes: use real_fake_trans_A by zeroing the second channel

---
+++ config/train_epithelial/v_3_0_6.py
@@ -0,0 +1,108 @@
+class GeneratorConfig:
+    unet_1x2x2_kernel_scale = True
+    unet_extra_xy_conv = False
+
+class DiscriminatorConfig:
+    disc_1x2x2_kernel_scale = False
+    disc_extra_xy_conv = False
+    disc_no_decrease_last_layers = False
+
+class Config:
+    batch_size = 64
+    beta1 = 0.5
+    border_offset_A = [0, 5, 18]
+    border_offset_B = [0, 0, 0]
+    checkpoints_dir = './checkpoints'
+    continue_train = False
+    crop_size = 256
+    dataroot = './datasets/epithelial_sheet'
+    datasetA_file = 'real_image.h5'
+    datasetA_mask = 'mask'
+    datasetA_names = None
+    datasetB_file = 'epithelial_v_1_2_0.h5'
+    datasetB_mask = None
+    datasetB_names = None
+    dataset_length = 'min'
+    dataset_mode = 'unaligned_3d'
+    dataset_stride = None
+    dataset_transforms_A = [
+        {'name': 'RandomDiscreteRotation', 'angles': [0, 90, 180, 270]},
+        {'name': 'RandomFlip'},
+    ]
+    dataset_transforms_B = [
+        {'name': 'RandomDiscreteRotation', 'angles': [0, 90, 180, 270]},
+        {'name': 'RandomFlip'},
+        {'name': 'RandomGaussianNoise', 'mean': 0, 'std': 0.2, 'slice_string': '1:2'},
+    ]
+    direction = 'AtoB'
+    disc_transforms_A = None
+    disc_transforms_B = None
+    discriminator_config = DiscriminatorConfig()
+    display_env = 'main'
+    display_freq = 9_984
+    display_id = -1
+    display_ncols = 4
+    display_port = 8097
+    display_server = 'http://localhost'
+    display_winsize = 256
+    element_size_um = [0.5, 0.2, 0.2]
+    epoch = 'latest'
+    epoch_count = 1
+    gan_mode = 'lsgan'
+    generator_config = GeneratorConfig()
+    generator_output_range = (-1, 1)
+    gpu_ids = [0]
+    init_gain = 0.02
+    init_type = 'normal'
+    input_nc = 1
+    input_value_range = (0, 255)
+    isTrain = True
+    lambda_A = 10.0
+    lambda_B = 10.0
+    lambda_identity = 0
+    load_iter = 0
+    load_size = 286
+    lr = 0.0002
+    lr_decay_iters = 50
+    lr_policy = 'linear'
+    max_dataset_size = float('inf')
+    model = 'cycle_gan_3d'
+    n_epochs = 50
+    n_epochs_decay = 50
+    n_layers_D = 3
+    name = 'epithelial_sheets_v_3_0_6'
+    ndf = 64
+    netD = 'basic'
+    netG = 'unet_32x64x64'
+    ngf = 64
+    no_dropout = True
+    no_flip = False
+    no_html = False
+    no_normalization = False
+    norm = 'batch'
+    num_threads = 4
+    output_nc = 2
+    phase = 'train'
+    pool_size = 50
+    post_transforms_A = [
+        {'name' : 'Threshold', 'lower': -1, 'upper': 1, 'slice_string': '0:1'},
+    ]
+    post_transforms_B = None
+    preprocess = 'resize_and_crop'
+    print_freq = 128
+    real_fake_trans_A = [
+        {'name': 'SetValue', 'value': -1, 'slice_string': '1:2'},
+    ]
+    real_fake_trans_B = None
+    sample_size = [32, 64, 64]
+    save_by_iter = False
+    save_epoch_freq = 5
+    save_latest_freq = 5000
+    serial_batches = False
+    suffix = ''
+    update_html_freq = display_freq
+    use_wandb = False
+    verbose = False
+    wandb_project_name = 'CycleGAN-and-pix2pix'
+
+config = Config()
