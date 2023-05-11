ebed33339136c20088b2d49fbcb18bd2bffb39dc
JlsBssmnn <julius.bossmann@mailbox.tu-dresden.de>
2023-05-11 13:11:25 +0200

    fix network_3d tests

--- test/model/networks_3d_test.py
+++ test/model/networks_3d_test.py
@@ -3,7 +3,8 @@ import unittest
 import torch
 import torch.nn as nn
 
-from models.networks_3d import ResnetGenerator, get_post_transform, UnetGenerator, NLayerDiscriminator, Scaler
+from models.networks_3d import ResnetGenerator, UnetGenerator, NLayerDiscriminator
+from models.custom_transforms import Scaler
 
 class Resnet9Blocks3dTest(unittest.TestCase):
 
@@ -32,59 +33,28 @@ class Resnet9Blocks3dTest(unittest.TestCase):
     self.assertEqual(out.shape, expected_shape)
 
 
-class PostTransformTest(unittest.TestCase):
-  def test_identity(self):
-    transform = get_post_transform('identity')
-
-    t = torch.tensor([-0.2, 0.5, 2, 8.41, -13.20])
-    transformed_t = transform(t)
-    self.assertTrue((t == transformed_t).all())
-
-    t = torch.rand(100)
-    transformed_t = transform(t)
-    self.assertTrue((t == transformed_t).all())
-
-  def test_map_binary(self):
-    transform = get_post_transform('threshold', -14, 14)
-
-    t = torch.tensor([-0.2, 0.5, 2, 8.41, -13.20])
-    transformed_t = transform(t)
-    self.assertTrue((transformed_t == torch.tensor([-14, 14, 14, 14, -14])).all())
-    self.assertFalse((transformed_t == t).all())
-
-    t = torch.rand(100)
-    transformed_t = transform(t)
-    self.assertTrue((transformed_t == torch.sign(t) * 14).all())
-
-    transform = get_post_transform('threshold', 4, 10)
-
-    t = torch.tensor([4, 6, 5.3, 8.3, 10, 8.9, 7, 6.99])
-    transformed_t = transform(t)
-    self.assertTrue((transformed_t == torch.tensor([4, 4, 4, 10, 10, 10, 10, 4])).all())
-
-
 class UnetGenerator3dTest(unittest.TestCase):
   def test_correct_shape(self):
     norm_layer = functools.partial(nn.InstanceNorm3d, affine=False, track_running_stats=False)
     net = UnetGenerator(1, 1, 5, norm_layer=norm_layer, use_dropout=True)
 
-    net_input = torch.rand((1, 32, 64, 64))
+    net_input = torch.rand((1, 1, 32, 64, 64))
     net_output = net(net_input)
-    self.assertEqual(net_output.shape, (1, 32, 64, 64))
+    self.assertEqual(net_output.shape, (1, 1, 32, 64, 64))
     self.assertEqual(net_output.dtype, torch.float32)
     self.assertLessEqual(net_output.max(), 1)
     self.assertGreaterEqual(net_output.min(), -1)
 
-    net_input = torch.rand((1, 32, 32, 32))
+    net_input = torch.rand((1, 1, 32, 32, 32))
     net_output = net(net_input)
-    self.assertEqual(net(net_input).shape, (1, 32, 32, 32))
+    self.assertEqual(net(net_input).shape, (1, 1, 32, 32, 32))
     self.assertEqual(net_output.dtype, torch.float32)
     self.assertLessEqual(net_output.max(), 1)
     self.assertGreaterEqual(net_output.min(), -1)
 
-    net_input = torch.rand((1, 64, 128, 64))
+    net_input = torch.rand((1, 1, 64, 128, 64))
     net_output = net(net_input)
-    self.assertEqual(net(net_input).shape, (1, 64, 128, 64))
+    self.assertEqual(net(net_input).shape, (1, 1, 64, 128, 64))
     self.assertEqual(net_output.dtype, torch.float32)
     self.assertLessEqual(net_output.max(), 1)
     self.assertGreaterEqual(net_output.min(), -1)
@@ -114,9 +84,9 @@ class UnetGenerator3dTest(unittest.TestCase):
     norm_layer = functools.partial(nn.InstanceNorm3d, affine=False, track_running_stats=False)
     net = UnetGenerator(1, 1, 6, norm_layer=norm_layer, use_dropout=True, unet_1x2x2_kernel_scale=True, unet_extra_xy_conv=True)
 
-    net_input = torch.rand((1, 32, 64, 64))
+    net_input = torch.rand((1, 1, 32, 64, 64))
     net_output = net(net_input)
-    self.assertEqual(net_output.shape, (1, 32, 64, 64))
+    self.assertEqual(net_output.shape, (1, 1, 32, 64, 64))
     self.assertEqual(net_output.dtype, torch.float32)
     self.assertLessEqual(net_output.max(), 1)
     self.assertGreaterEqual(net_output.min(), -1)
@@ -141,7 +111,7 @@ class UnetGenerator3dTest(unittest.TestCase):
     norm_layer = functools.partial(nn.InstanceNorm3d, affine=False, track_running_stats=False)
     net = UnetGenerator(1, 1, 5, norm_layer=norm_layer, use_dropout=True)
 
-    net_input = torch.rand((1, 32, 64, 64))
+    net_input = torch.rand((1, 1, 32, 64, 64))
 
     inner_shape = None
     next_shape = None
