8ee12e370d8aef925c3f47a8f5c78a6a66035dca
JlsBssmnn <julius.bossmann@mailbox.tu-dresden.de>
2023-06-12 21:55:02 +0200

    use GeneratorApplier class; move images to gpu when using gpu

--- util/Evaluater.py
+++ util/Evaluater.py
@@ -12,7 +12,7 @@ class EpithelialEvaluater:
     def __init__(self, config):
         sys.path.append(str(pathlib.Path(__file__).parent.parent.parent))
         global apply_generator, Evaluater
-        from evaluation.translate_image import apply_generator
+        from evaluation.translate_image import GeneratorApplier
         from evaluation.evaluate_image import Evaluater
 
         self.config = config.evaluation_config
@@ -25,14 +25,25 @@ class EpithelialEvaluater:
                 self.cell_truths.append(np.asarray(f[datasets[1]]))
 
         self.images = []
+        image_shape = None
         transform = Scaler(config.input_value_range[0], config.input_value_range[1], -1, 1)
         with h5py.File(self.config.input_file) as f:
             for s in self.config.image_slices:
                 image = np.asarray(eval(f"f[self.config.input_dataset][{s}]"))
                 image = torch.tensor(image)
                 image = transform(image)
+                if self.config.use_gpu:
+                    image = image.cuda(config.gpu_ids[0])
+
+                if image_shape is None:
+                    image_shape = image.shape
+                else:
+                    assert image_shape == image.shape, "All evaluation image must have the same shape"
+
                 self.images.append(image)
+
         self.image_names = config.evaluation_config.image_names
+        self.generator_applier = GeneratorApplier(self.images[0].shape, self.config)
         self.evaluater = Evaluater(self.config, False)
         self.net_out_transform = Scaler(config.generator_output_range[0], config.generator_output_range[1], 0, 1)
 
@@ -42,7 +53,7 @@ class EpithelialEvaluater:
         outputs = []
         generator.eval()
         for image in self.images:
-            output = apply_generator(image, generator, self.config)
+            output = self.generator_applier.apply_generator(image, generator)
             output = self.net_out_transform(torch.from_numpy(output)).numpy()
             outputs.append(output)
         generator.train()
