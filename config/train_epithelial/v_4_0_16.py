8cd934ed696ae048ea5ec69058caa730569cbfc5
JlsBssmnn <julius.bossmann@mailbox.tu-dresden.de>
2023-07-01 15:25:27 +0200

    create config v4.0.16
    changes: use wgangp loss

---
+++ config/train_epithelial/v_4_0_16.py
@@ -0,0 +1,155 @@
+from util.visualizing_transforms import ToUint8
+
+output_nc = 1
+
+class GeneratorConfig:
+    unet_1x2x2_kernel_scale = True
+    unet_extra_xy_conv = False
+    last_activation = 'tanh'
+
+class DiscriminatorConfig:
+    disc_1x2x2_kernel_scale = False
+    disc_extra_xy_conv = False
+    disc_no_decrease_last_layers = False
+    last_activation = None
+
+class EvaluationConfig:
+    class GeneratorConfig:
+        output_nc = output_nc
+
+    eval_freq = 512
+    slice_str = '0, 16'
+    membrane_black = False
+    ground_truth_file = './datasets/epithelial_sheet/evaluation/ground_truth.h5'
+    ground_truth_datasets = [('slice1_membrane_truth', 'slice1_cell_truth'),
+                             ('slice2_membrane_truth', 'slice2_cell_truth'),
+                             ('slice3_membrane_truth', 'slice3_cell_truth')]
+    image_names = ['slice1', 'slice2', 'slice3']
+    input_file = './datasets/epithelial_sheet/trainA/real_image.h5'
+    input_dataset = 'image'
+    image_slices = [':, 23:55, 1034:1546, 1031:1543', ':, 31:63, 1941:2453, 54:566', ':, 52:84, 35:547, 2029:2541']
+    patch_size = (32, 64, 64)
+    generator_config = GeneratorConfig()
+    use_gpu = True
+    scale_with_patch_max = False
+    
+    batch_size = 128
+    basins_range = (0, 40, 10)
+    membrane_range = (100, 256, 50)
+    error_factor = 1/2
+    show_progress = False
+    local_error_measure = 'acc'
+    local_error_a = 0.5
+    local_error_b = 3.5
+    vi_freq = 16384
+    scale_with_patch_max = False
+
+class Config:
+    batch_size = 64
+    beta1 = 0.5
+    border_offset_A = [0, 5, 18]
+    border_offset_B = [0, 0, 0]
+    checkpoints_dir = './checkpoints'
+    continue_train = False
+    crop_size = 256
+    dataroot = './datasets/epithelial_sheet'
+    datasetA_creation_func = None
+    datasetA_file = 'real_image.h5'
+    datasetA_mask = 'mask'
+    datasetB_creation_func = None
+    datasetA_names = None
+    datasetA_random_sampling = True
+    datasetB_file = 'epithelial_v_1_3_0.h5'
+    datasetB_mask = None
+    datasetB_names = None
+    datasetB_random_sampling = True
+    dataset_length = 'min'
+    dataset_mode = 'unaligned_3d'
+    dataset_stride = None
+    dataset_transforms_A = [
+        {'name': 'RandomDiscreteRotation', 'angles': [0, 90, 180, 270]},
+        {'name': 'RandomFlip'},
+    ]
+    dataset_transforms_B = [
+        {'name': 'RandomDiscreteRotation', 'angles': [0, 90, 180, 270]},
+        {'name': 'RandomFlip'},
+        {'name': 'ChannelModifier', 'x': 0, 'channels_to_delete': [1]},
+    ]
+    direction = 'AtoB'
+    disc_transforms_A = [
+        {'name': 'GaussianBlur', 'channels': output_nc, 'kernel_size': [1, 3, 3], 'sigma': 0.5},
+    ]
+    disc_transforms_B = None
+    discriminator_config = DiscriminatorConfig()
+    display_env = 'main'
+    display_freq = 9_984
+    display_id = -1
+    display_ncols = 4
+    display_port = 8097
+    display_server = 'http://localhost'
+    display_transform_A = ToUint8
+    display_transform_B = ToUint8
+    display_winsize = 256
+    element_size_um = [0.5, 0.2, 0.2]
+    epoch = 'latest'
+    epoch_count = 1
+    evaluation_config = EvaluationConfig()
+    extra_losses = []
+    gan_mode = 'wgangp'
+    generator_config = GeneratorConfig()
+    generator_output_range = (-1, 1)
+    gpu_ids = [0]
+    init_gain = 0.02
+    init_type = 'normal'
+    input_nc = 1
+    input_value_range = (0, 255)
+    isTrain = True
+    lambda_A = 5.0
+    lambda_B = 5.0
+    lambda_gp = 10
+    lambda_identity = 0
+    load_iter = 0
+    load_size = 286
+    lr_D = 0.0002
+    lr_G = 0.0002
+    lr_decay_iters = 50
+    lr_policy = 'linear'
+    max_dataset_size = 2_000
+    model = 'cycle_gan_3d'
+    n_epochs = 50
+    n_epochs_decay = 50
+    n_layers_D = 3
+    name = 'epithelial_sheets_v_4_0_16'
+    ndf = 64
+    netD = 'basic'
+    netG = 'unet_32x64x64'
+    ngf = 64
+    no_adversarial_loss = False
+    no_dropout = True
+    no_flip = False
+    no_html = False
+    no_normalization = False
+    norm = 'batch'
+    num_threads = 1
+    output_nc = output_nc
+    phase = 'train'
+    pool_size = 50
+    post_transforms_A = disc_transforms_A
+    post_transforms_B = None
+    preprocess = 'resize_and_crop'
+    print_freq = 128
+    real_fake_trans_A = None
+    real_fake_trans_B = None
+    sample_size = [32, 64, 64]
+    save_by_iter = False
+    save_epoch_freq = 50
+    save_latest_freq = 5000
+    serial_batches = False
+    suffix = ''
+    update_html_freq = display_freq
+    use_tensorboard = False
+    use_wandb = False
+    verbose = False
+    wandb_project_name = 'CycleGAN-and-pix2pix'
+
+config = Config()
